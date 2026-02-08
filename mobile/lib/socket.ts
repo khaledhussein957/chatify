a15f6fd572a02366af6499dbbf8cfc358cb65861
Khaled Hussein <khaledhussein957@gmail.com>
2026-02-08 16:33:59 +0300

    update socket on mobile folder

--- mobile/lib/socket.ts
+++ mobile/lib/socket.ts
@@ -4,7 +4,7 @@ import { QueryClient } from "@tanstack/react-query";
 import { Chat, Message, MessageSender, Status } from "@/types";
 import { useAuthStore } from "@/store/auth";
 
-const SOCKET_URL = "https://chatify-server-hazel.vercel.app/api";
+const SOCKET_URL = "https://chatify-server-hazel.vercel.app";
 
 interface SocketState {
   socket: Socket | null;
