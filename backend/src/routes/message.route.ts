c066c41b326b98de05c1639ad4c3d61ae24e3539
Khaled Hussein <khaledhussein957@gmail.com>
2026-02-10 17:17:28 +0300

    feat: Add message routes for sending text and voice messages, retrieving, updating, and deleting messages.

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 import {
   getMessages,
   sendMessage,
-  sendMessageWithContent,
+  // sendMessageWithContent,
   updateTextMessage,
   deleteMessage,
   sendVoiceMessage,
